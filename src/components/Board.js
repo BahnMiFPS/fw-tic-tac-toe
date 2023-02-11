03db1a7e89f84620ca01578a8b9dabc7083ac13d
BahnMiFPS <87252277+BahnMiFPS@users.noreply.github.com>
2023-02-11 14:03:32 +1100

    1

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,14 +1,24 @@
-import React, { useState } from "react";
-import Square from "./Square";
+import React, { useState } from "react"
+import Square from "./Square"
 
 export default function Board({ squares, handleClick }) {
-  return (
-    <div className="board">
-      <div>
-        <div className="board-row">"Your code here"</div>
-        <div className="board-row">"Your code here"</div>
-        <div className="board-row">"Your code here"</div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="board">
+			<div>
+				<div className="board-row">"Your code here"</div>
+				<div className="board-row">"Your code here"</div>
+				<div className="board-row">"Your code here"</div>
+			</div>
+			<div>
+				<div className="board-row">"Your code here"</div>
+				<div className="board-row">"Your code here"</div>
+				<div className="board-row">"Your code here"</div>
+			</div>
+			<div>
+				<div className="board-row">"Your code here"</div>
+				<div className="board-row">"Your code here"</div>
+				<div className="board-row">"Your code here"</div>
+			</div>
+		</div>
+	)
 }
