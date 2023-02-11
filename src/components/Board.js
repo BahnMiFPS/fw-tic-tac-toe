a5bcd488885f1c0603fade6a2248f3edb25b5484
BahnMiFPS <87252277+BahnMiFPS@users.noreply.github.com>
2023-02-11 14:23:43 +1100

    43

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,15 +6,20 @@ export default function Board({ squares, handleClick }) {
 		<div className="board">
 			<div>
 				<div className="board-row">
-					<Square
-						value={squares[0]}
-						handleClick={() => {
-							handleClick(0)
-						}}
-					/>
+					<Square value={squares[0]} handleClick={() => handleClick(0)} />
+					<Square value={squares[1]} handleClick={() => handleClick(1)} />
+					<Square value={squares[2]} handleClick={() => handleClick(2)} />
 				</div>
-				<div className="board-row">"Your code here"</div>
-				<div className="board-row">"Your code here"</div>
+				<div className="board-row">
+					<Square value={squares[3]} handleClick={() => handleClick(0)} />
+					<Square value={squares[4]} handleClick={() => handleClick(4)} />
+					<Square value={squares[2]} handleClick={() => handleClick(2)} />
+				</div>
+				<div className="board-row">
+					<Square value={squares[6]} handleClick={() => handleClick(6)} />
+					<Square value={squares[7]} handleClick={() => handleClick(7)} />
+					<Square value={squares[8]} handleClick={() => handleClick(8)} />
+				</div>
 			</div>
 		</div>
 	)
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,65 +1,67 @@
-import React, { useState, useEffect } from "react";
-import Board from "./Board";
+import React, { useState, useEffect } from "react"
+import Board from "./Board"
 
 function Game() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-  const [winner, setWinner] = useState(null);
+	const [squares, setSquares] = useState(Array(9).fill(null))
+	const [xIsNext, setXIsNext] = useState(true)
+	const [winner, setWinner] = useState(null)
 
-  //Declaring a Winner
-  useEffect(() => {
-    "Your code here";
-  }, [squares]);
+	//Declaring a Winner
+	useEffect(() => {
+		"Your code here"
+	}, [squares])
 
-  //function to check if a player has won.
-  //If a player has won, we can display text such as “Winner: X” or “Winner: O”.
-  //Input: squares: given an array of 9 squares:'X', 'O', or null.
-  const calculateWinner = (squares) => {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        return squares[a];
-      }
-    }
-    return null;
-  };
+	//function to check if a player has won.
+	//If a player has won, we can display text such as “Winner: X” or “Winner: O”.
+	//Input: squares: given an array of 9 squares:'X', 'O', or null.
+	const calculateWinner = (squares) => {
+		const lines = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6],
+		]
+		for (let i = 0; i < lines.length; i++) {
+			const [a, b, c] = lines[i]
+			if (
+				squares[a] &&
+				squares[a] === squares[b] &&
+				squares[a] === squares[c]
+			) {
+				return squares[a]
+			}
+		}
+		return null
+	}
 
-  //Handle player
-  const handleClick = (i) => {
-    "Your code here";
-  };
+	//Handle player
+	const handleClick = (i) => {
+		//check X or O
+		// calculate winner
+		// declare winner
+	}
 
-  //Restart game
-  const handlRestart = () => {
-    "Your code here";
-  };
+	//Restart game
+	const handlRestart = () => {
+		"Your code here"
+	}
 
-  return (
-    <div className="main">
-      <h2 className="result">Winner is: {winner ? winner : "N/N"}</h2>
-      <div className="game">
-        <span className="player">Next player is: {xIsNext ? "X" : "O"}</span>
-        <Board squares={"Your code here"} handleClick={"Your code here"} />
-      </div>
-      <button onClick={"Your code here"} className="restart-btn">
-        Restart
-      </button>
-    </div>
-  );
+	return (
+		<div className="main">
+			<h2 className="result">Winner is: {winner ? winner : "N/N"}</h2>
+			<div className="game">
+				<span className="player">Next player is: {xIsNext ? "X" : "O"}</span>
+				<Board squares={squares} handleClick={handleClick(squares)} />
+			</div>
+			<button onClick={"Your code here"} className="restart-btn">
+				Restart
+			</button>
+		</div>
+	)
 }
 
-export default Game;
+export default Game
